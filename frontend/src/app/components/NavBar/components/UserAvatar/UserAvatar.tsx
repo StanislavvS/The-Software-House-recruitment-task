97032233b535615be22533158edde6db11d013c6
Stanislaw Sobczak <sstanislaw85@gmail.com>
2022-04-25 08:22:07 +0200

    Style logout tooltip

--- frontend/src/app/components/NavBar/components/UserAvatar/UserAvatar.tsx
+++ frontend/src/app/components/NavBar/components/UserAvatar/UserAvatar.tsx
@@ -1,15 +1,34 @@
 import { useAuth } from "providers/auth-context";
 import AvatarImage from "../../../../../img/Avatar.png";
 import { useAvatarStyles } from "./styles";
-import React from "react";
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css";
+import { useRef } from "react";
 
 const UserAvatar = () => {
   const { user } = useAuth();
   const classes = useAvatarStyles();
+  const ref = useRef();
 
   return (
     <>
-      <img className={classes.avatar} src={AvatarImage} alt="userAvatr" />
+      <Tippy
+        className={classes.tooltip}
+        interactive={true}
+        content={
+          <button
+            className={classes.tooltipButton}
+            onClick={() => {
+              console.log("dupa");
+            }}
+          >
+            Logout
+          </button>
+        }
+        arrow={false}
+      >
+        <img className={classes.avatar} src={AvatarImage} alt="userAvatar" />
+      </Tippy>
     </>
   );
 };
