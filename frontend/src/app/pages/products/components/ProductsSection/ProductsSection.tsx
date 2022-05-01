096f9c9b1788cbc2444ed188dd024426c3c9ac4b
Stanislaw Sobczak <sstanislaw85@gmail.com>
2022-05-01 15:24:04 +0200

    Style paggination component and fix styles products section

--- frontend/src/app/pages/products/components/ProductsSection/ProductsSection.tsx
+++ frontend/src/app/pages/products/components/ProductsSection/ProductsSection.tsx
@@ -3,13 +3,14 @@ import ProductCard from "app/pages/products/components/ProductCard/ProductCard";
 import { useProducts } from "providers/ProductsProvider";
 import { Product } from "providers/ProductsProvider.types";
 import React, { useState, useEffect } from "react";
+import ReactPaginate from "react-paginate";
 import { useProductsPage } from "../../hooks";
 import { useProductsSectionStyles } from "./styles";
-import { Pagination } from "antd";
 
 const ProductsSection = () => {
   const { isLoading } = useProductsPage();
   const { products, filtersOption } = useProducts();
+  const [selectedPage, setSelectedPage] = useState<number>(0);
   const classes = useProductsSectionStyles();
   const [filterProducts, setFilterProducts] = useState<Product[]>([]);
 
@@ -31,50 +32,49 @@ const ProductsSection = () => {
   if (isLoading) return <Spinner />;
 
   return (
-    <div className={classes.productsSectionContainer}>
-      {filtersOption.active ||
-      filtersOption.promo ||
-      filtersOption.filterTextValue !== ""
-        ? filterProducts.map(
-            ({ id, name, description, promo, active, image, rating }) => (
-              <ProductCard
-                key={id}
-                id={id}
-                name={name}
-                description={description}
-                promo={promo}
-                active={active}
-                image={image}
-                rating={rating}
-              />
+    <div className={classes.productSectionContainer}>
+      <div className={classes.productsSection}>
+        {filtersOption.active ||
+        filtersOption.promo ||
+        filtersOption.filterTextValue !== ""
+          ? filterProducts.map(
+              ({ id, name, description, promo, active, image, rating }) => (
+                <ProductCard
+                  key={id}
+                  id={id}
+                  name={name}
+                  description={description}
+                  promo={promo}
+                  active={active}
+                  image={image}
+                  rating={rating}
+                />
+              )
             )
-          )
-        : products.map(
-            ({ id, name, description, promo, active, image, rating }) => (
-              <ProductCard
-                key={id}
-                id={id}
-                name={name}
-                description={description}
-                promo={promo}
-                active={active}
-                image={image}
-                rating={rating}
-              />
-            )
-          )}
-
-      <Pagination
-        defaultCurrent={1}
-        total={
-          filtersOption.active ||
-          filtersOption.promo ||
-          filtersOption.filterTextValue !== ""
-            ? filterProducts.length
-            : products.length
-        }
-        defaultPageSize={8}
-        showSizeChanger={false}
+          : products.map(
+              ({ id, name, description, promo, active, image, rating }) => (
+                <ProductCard
+                  key={id}
+                  id={id}
+                  name={name}
+                  description={description}
+                  promo={promo}
+                  active={active}
+                  image={image}
+                  rating={rating}
+                />
+              )
+            )}
+      </div>
+      <ReactPaginate
+        className={classes.productSectionPagination}
+        pageCount={products.length / 8}
+        onPageChange={(value) => setSelectedPage(value.selected)}
+        pageRangeDisplayed={selectedPage === 1 ? 2 : 3}
+        marginPagesDisplayed={2}
+        nextLabel=""
+        previousLabel=""
+        breakLabel="..."
       />
     </div>
   );
--- frontend/src/app/pages/products/components/ProductsSection/styles.ts
+++ frontend/src/app/pages/products/components/ProductsSection/styles.ts
@@ -1,11 +1,32 @@
 import { createUseStyles } from "react-jss";
+import { colors } from "styles/variables";
 
 export const useProductsSectionStyles = createUseStyles({
-  productsSectionContainer: {
-    padding: "10% 8% 10% 8%",
+  productsSection: {
+    padding: "10% 8% 10% 10%",
     gridTemplateColumns: "23.5% 23.5% 23.5% 23.5%",
     gridTemplateRows: "3.75% 3.75% 3.75% 3.75%",
     gap: "1.5rem",
     display: "grid",
+  },
+
+  productSectionContainer: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+
+  productSectionPagination: {
+    listStyle: "none",
+    height: "20%",
+    fontWeight: "bold",
+    margin: "auto",
+    "& a": {
+      color: colors.eerieBlack,
+    },
+    display: "flex",
+    justifyContent: "space-between",
+    paddingBottom: "5%",
+    width: "20%",
   },
 });
